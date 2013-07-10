c5a187c23f07e05f0a277f01d6cf19a32a778008
Micah Walter <mwalter@haverford.edu>
2013-07-10 17:57:57 -0400

    Revised parameters, support for arranger.py, support for testing

--- transform/transform.py
+++ transform/transform.py
@@ -1,15 +1,21 @@
 import clefs
+import arranger
 from constants import *
 from pymei import XmlImport, XmlExport
 
 class TransformData:
-	def __init__(self, orig_clefs=EMPTY_CLEFS):
+	def __init__(self, orig_clefs=EMPTY_CLEFS, arranger_editor=False):
 		self.orig_clefs = orig_clefs
+
+def TEST_SET_UP(data):
+	"""Mutates data to test specific transformations"""
+	data.arranger_editor = True
 
 def transform(MEI_doc, data=TransformData()):
 	new_doc = MEI_doc
-	new_doc_root = new_doc.getRootElement()
-	new_doc_root = clefs.clefs(new_doc_root, data.orig_clefs)
+	tree = new_doc.getRootElement()
+	clefs.clefs(tree, data.orig_clefs)
+	arranger.arranger(tree, data.arranger_editor)
 	return new_doc
 
 def ui():
@@ -18,6 +24,7 @@ def ui():
 		old_filename += '.mei'
 	old_MEI_doc = XmlImport.documentFromFile(old_filename)
 	data = TransformData() # Will be filled in
+	TEST_SET_UP(data) # For testing purposes
 	new_MEI_doc = transform(old_MEI_doc, data)
 	new_filename = old_filename[:-4] + '_.mei'
 	status = XmlExport.meiDocumentToFile(new_MEI_doc, new_filename)
