e4d5f4c7e9bafb5799d82ca4b9d99a967fa3bef8
Micah Walter <mwalter@haverford.edu>
2013-07-11 15:24:25 -0400

    Beginnings of analyze function

---
+++ analyze/staff_info.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(0, '..')
+
+from constants import *
+from pymei import XmlImport
+
+def orig_clefs_missing(MEI_tree):
+	all_staffGrp = MEI_tree.getDescendantsByName('staffGrp')
+	for staffGrp in all_staffGrp:
+		# Only check if not part of <orig> or <reg> to begin with
+		if staffGrp.getParent().getName() not in [ORIG, REG]:
+			if not has_C_clef(staffGrp):
+				return True
+	return False
+
+if __name__ == "__main__":
+	pass
