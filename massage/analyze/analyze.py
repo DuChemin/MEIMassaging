5b4eab93b61608fa26c12ac70a5b6228d26dad17
Micah Walter <mwalter@haverford.edu>
2013-07-11 17:41:08 -0400

    analyze tested for staff_names and orig_clefs_missing.

--- massage/analyze/analyze.py
+++ massage/analyze/analyze.py
@@ -1,19 +1,31 @@
 
-import staff_info
-import orig_clefs_missing
 from pymei import XmlImport
+from staff_names import staff_names
+from orig_clefs_missing import orig_clefs_missing
 
 class AnalyzeData:
-	def __init__(self, staff_info, orig_clefs_missing):
-		self.staff_info = staff_info
+	def __init__(self, staff_names, orig_clefs_missing):
+		self.staff_names = staff_names
 		self.orig_clefs_missing = orig_clefs_missing
+
+	def __repr__(self):
+		r = ''
+		r += 'Staff names:\n'
+		for name in self.staff_names:
+			r += ' * '
+			r += name
+			r += '\n'
+		r += ('Original clefs ' + ('not ' if self.orig_clefs_missing else '') +
+		      'missing')
+		return r
 
 def analyze(MEI_filename):
 	MEI_doc = XmlImport.documentFromFile(MEI_filename)
 	MEI_tree = MEI_doc.getRootElement()
-	staff_info = staff_info.staff_info(MEI_doc)
-	orig_clefs_missing = orig_clefs_missing.orig_clefs_missing(MEI_doc)
-	return AnalyzeData(staff_info, orig_clefs_missing)
+	this_staff_names = staff_names(MEI_tree)
+	this_orig_clefs_missing = orig_clefs_missing(MEI_tree)
+	return AnalyzeData(this_staff_names, this_orig_clefs_missing)
 
 if __name__ == "__main__":
-	pass
+	# For testing
+	print(analyze('minimal.mei'))
