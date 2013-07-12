868110e07293c004d7579cffbdb9d20f29ebb680
Micah Walter <mwalter@haverford.edu>
2013-07-12 17:56:51 -0400

    Updated analysis

--- massage/analyze/analyze.py
+++ massage/analyze/analyze.py
@@ -1,18 +1,25 @@
 
-import analyze.staff_info
-import analyze.orig_clefs_missing
-# from pymei import XmlImport
+from first_measure_empty import first_measure_empty
+from orig_clefs_missing import orig_clefs_missing
+from staff_info import staff_info
+
+from pymei import XmlImport
 
 class AnalyzeData:
-	def __init__(self, staff_info, orig_clefs_missing):
+	def __init__(self,
+	             first_measure_empty,
+	             staff_info,
+	             orig_clefs_missing):
+		self.first_measure_empty = first_measure_empty
 		self.staff_info = staff_info
 		self.orig_clefs_missing = orig_clefs_missing
 
 def analyze(MEI_filename):
 	MEI_doc = XmlImport.documentFromFile(MEI_filename)
 	MEI_tree = MEI_doc.getRootElement()
-	staff_info = staff_info.staff_info(MEI_doc)
-	orig_clefs_missing = orig_clefs_missing.orig_clefs_missing(MEI_doc)
+	first_measure_empty = first_measure_empty(MEI_doc)
+	orig_clefs_missing = orig_clefs_missing(MEI_doc)
+	staff_info = staff_info(MEI_doc)
 	return AnalyzeData(staff_info, orig_clefs_missing)
 
 if __name__ == "__main__":
@@ -32,3 +39,5 @@ if __name__ == "__main__":
 #         | |  |  ``/  /`  /
 #        /,_|  |   /,_/   /
 #           /,_/      '`-'
+#
+#      E A S T E R   P I G
---
+++ massage/analyze/first_measure_empty.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.insert(0, '..')
+
+from constants import *
+
+def first_measure_empty(MEI_tree):
+	# Add code here!
+	return False
+
+if __name__ == "__main__":
+	pass
