2aba20468457383124146c75ee8d85827fa16b14
Micah Walter <mwalter@haverford.edu>
2013-07-22 10:56:12 -0400

    Measure renumbering work

--- massage/analyze/first_measure_empty.py
+++ massage/analyze/first_measure_empty.py
@@ -4,8 +4,16 @@ sys.path.insert(0, '..')
 from constants import *
 
 def first_measure_empty(MEI_tree):
-	# Add code here!
-	return False
+	all_measures = MEI_tree.getDescendantsByName('measure')
+	first_measure = all_measures[0]
+	# Gets all the staves in the first measure
+	first_staves = first_measure.getChildrenByName('staff')
+	# If all of those are empty, return True;
+	# if one is not empty, return False.
+	for staff in first_staves:
+		if staff.getChildren() != []:
+			return False
+	return True
 
 if __name__ == "__main__":
 	pass
--- massage/transform/incipit.py
+++ massage/transform/incipit.py
@@ -3,11 +3,15 @@ from constants import *
 # from pymei import MeiElement
 
 def obliterate_incipit(MEI_tree):
-	# all_measures = MEI_tree.getDescendantsByName('measure')
-	# last_measure = all_measures[-1]
-	# last_staves = last_measure.getChildren()
-	# for staff in last_staves:
-	# 	all_last_notes = staff.getDescendantsByName('note')
-	# 	last_note_of_staff = all_last_notes[-1]
-	# 	# The following line *replaces* the existing @dur.
-	# 	last_note_of_staff.addAttribute('dur', LONGA)
+	all_measures = MEI_tree.getDescendantsByName('measure')
+	first_measure = all_measures[0]
+	first_measure.getParent().removeChild(first_measure)
+
+def renumber_measures(MEI_tree):
+	all_measures = MEI_tree.getDescendantsByName('measure')
+	for measure in all_measures:
+		# Get measure number attribute
+		attr_measure_number = measure.getAttribute('n')
+		val_measure_number = attr_measure_number.getValue()
+		# Reduce number by one
+		attr_measure_number.setValue(val_measure_number - 1)
