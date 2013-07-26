2a27df5f08216886daeea21b0541760af0f4c0d6
Phillip Lu <plu@haverford.edu>
2013-07-26 16:34:43 -0400

    No more upload by date.

--- massage/massage/frontEnd/models.py
+++ massage/massage/frontEnd/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Document(models.Model):
 #	name = models.CharField(max_length = 200)
-	docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+	docfile = models.FileField(upload_to='documents/')
 
 #class MEIDocument(models.Model):
 #	PROCESS_TYPE_CHOICES = (
