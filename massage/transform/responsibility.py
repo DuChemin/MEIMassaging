2abe28f926c6d7ccf2399ba81b64dd3b15e89805
Zoltan Komives <zolaemil@gmail.com>
2013-11-26 14:40:05 +0100

    replaced 'func' with 'reason' attribute

--- massage/transform/responsibility.py
+++ massage/transform/responsibility.py
@@ -5,6 +5,6 @@ def responsibility(MEI_tree, resp):
 	"""Adds @func and @resp attributes to editorial accidentals."""
 	all_supplied = MEI_tree.getDescendantsByName('supplied')
 	for supplied in all_supplied:
-		supplied.addAttribute('func', 'edit')
+		supplied.addAttribute('reason', 'edit')
 		if resp:
 			supplied.addAttribute('resp', resp)
