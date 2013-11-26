ad6d6212e7d2d5ff14de17236f8c314fe65e48c3
Zoltan Komives <zolaemil@gmail.com>
2013-11-26 18:48:56 +0100

    Added emendation DC test files
    … and regrouped test cases in transform_test.py

--- massage/test/transform_test.py
+++ massage/test/transform_test.py
@@ -29,6 +29,59 @@ class TestCase:
 		else:
 			print("Transformation failed")
 
+""" =========================================="""
+""" ========== SIMPLE TEST CASES ============= """
+
+name = 'TC_Variants.01'
+mei_file = 'dat/TC.Variants.01.mei'
+transform_data = TransformData()
+transform_data.arranger_to_editor = True
+transform_data.replace_longa = True
+transform_data.obliterate_incipit = False
+transform_data.editorial_resp = 'ZK'
+transform_data.alternates_list = [
+		('1', VARIANT, '1', ''),
+		('2', VARIANT, '1', 'ZK'),
+		('3', VARIANT, '1', 'ZK'),
+		('4', VARIANT, '1', 'ZK'), 
+		]
+test_cases.append(TestCase(name, mei_file, transform_data))
+
+name = 'TC_Variants.02 - Whole-Measure <app>'
+mei_file = 'dat/TC.Variants.02.WholeMeasure.mei'
+transform_data = TransformData()
+transform_data.arranger_to_editor = True
+transform_data.replace_longa = True
+transform_data.obliterate_incipit = False
+transform_data.editorial_resp = 'ZK'
+transform_data.alternates_list = [
+		('1', VARIANT, '1', ''),
+		('2', VARIANT, '1', 'ZK'),
+		('3', VARIANT, '1', 'ZK') 
+		]
+test_cases.append(TestCase(name, mei_file, transform_data))
+
+
+""" =========================================="""
+""" ========== DU CHEMIN TEST FILES ========== """
+
+name = 'TC_Emendations.DC0113'
+mei_file = 'dat/DC0113E.mei'
+transform_data = TransformData()
+transform_data.arranger_to_editor = True
+transform_data.replace_longa = True
+transform_data.obliterate_incipit = True
+transform_data.editorial_resp = 'ZK'
+transform_data.alternates_list = [
+		('1', VARIANT, '1', ''),
+		('2', VARIANT, '2', ''),
+		('3', EMENDATION, '2', 'Freedman'),
+		('4', VARIANT, '4', ''),
+		('5', VARIANT, '5', ''), 
+		('6', EMENDATION, '5', 'Freedman'), 
+		]
+test_cases.append(TestCase(name, mei_file, transform_data))
+
 
 name = 'TC_Reconstructions.DC1209'
 mei_file = 'dat/DC1209_Synoptic_Reconstruction-normalized.mei'
@@ -42,12 +95,12 @@ transform_data.alternates_list = [
 		('2', VARIANT, '2', ''),
 		('3', RECONSTRUCTION, '2', 'ZK'),
 		('4', RECONSTRUCTION, '2', 'ZK'),
-		('5', VARIANT, '5', 'ZK'), 
-		('6', VARIANT, '6', 'ZK'), 
+		('5', VARIANT, '5', ''), 
+		('6', VARIANT, '6', ''), 
 		('7', RECONSTRUCTION, '6', 'ZK'),
 		('8', RECONSTRUCTION, '6', 'ZK'),
 		]
-test_cases.append(TestCase(name, mei_file, transform_data))
+# test_cases.append(TestCase(name, mei_file, transform_data))
 
 name = 'TC_Reconstructions.DC1313'
 mei_file = 'dat/DC1313_Synoptic_Reconstruction-normalized.mei'
@@ -61,12 +114,12 @@ transform_data.alternates_list = [
 		('2', VARIANT, '2', ''),
 		('3', RECONSTRUCTION, '2', 'ZK'),
 		('4', RECONSTRUCTION, '2', 'ZK'),
-		('5', VARIANT, '5', 'ZK'), 
-		('6', VARIANT, '6', 'ZK'), 
+		('5', VARIANT, '5', ''), 
+		('6', VARIANT, '6', ''), 
 		('7', RECONSTRUCTION, '6', 'ZK'),
 		('8', RECONSTRUCTION, '6', 'ZK'),
 		]
-test_cases.append(TestCase(name, mei_file, transform_data))
+# test_cases.append(TestCase(name, mei_file, transform_data))
 
 name = 'TC_DC0221'
 mei_file = 'dat/DC0221_Synoptic_Variants.Voice_Type_Source.mei'
@@ -91,34 +144,6 @@ transform_data.alternates_list = [
 		('13', VARIANT, '11', 'ZK') ]
 #test_cases.append(TestCase(name, mei_file, transform_data))
 
-name = 'TC_Variants.01'
-mei_file = 'dat/TC.Variants.01.mei'
-transform_data = TransformData()
-transform_data.arranger_to_editor = True
-transform_data.replace_longa = True
-transform_data.obliterate_incipit = False
-transform_data.editorial_resp = 'ZK'
-transform_data.alternates_list = [
-		('1', VARIANT, '1', ''),
-		('2', VARIANT, '1', 'ZK'),
-		('3', VARIANT, '1', 'ZK'),
-		('4', VARIANT, '1', 'ZK'), 
-		]
-# test_cases.append(TestCase(name, mei_file, transform_data))
-
-name = 'TC_Variants.02 - Whole-Measure <app>'
-mei_file = 'dat/TC.Variants.02.WholeMeasure.mei'
-transform_data = TransformData()
-transform_data.arranger_to_editor = True
-transform_data.replace_longa = True
-transform_data.obliterate_incipit = False
-transform_data.editorial_resp = 'ZK'
-transform_data.alternates_list = [
-		('1', VARIANT, '1', ''),
-		('2', VARIANT, '1', 'ZK'),
-		('3', VARIANT, '1', 'ZK') 
-		]
-# test_cases.append(TestCase(name, mei_file, transform_data))
 
 
 for tc in test_cases:
