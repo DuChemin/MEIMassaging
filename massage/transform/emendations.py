186d5d2bd3d499d813e74878381f0682efd9592e
Zoltan Komives <zolaemil@gmail.com>
2013-11-27 11:07:32 +0100

    moved variant and emendation handling methods to alt.py

--- massage/transform/emendations.py
+++ massage/transform/emendations.py
@@ -1,5 +1,6 @@
 
 from constants import *
+from alt import local_alternatives
 # from pymei import MeiElement
 
 def emendations(MEI_tree, alternates_list):
@@ -7,15 +8,5 @@ def emendations(MEI_tree, alternates_list):
 	and reorganize the MEI file so that the alternate readings are
 	grouped together with the lemma.
 	"""
-	# See transform.py for documentation for the alternates_list object.
-	# emendation_list = [i for i in alternates_list
-	# 		if i[1] == EMENDATION and i[0] != i[2]]
-	# for measure in MEI_tree.getDescendantsByName('measure'):
-	# 	add_all_apps_in_measure2(measure, variants_list)
-	# 	remove_measure_var_staves(measure, variants_list)
-	# delete_staff_def(MEI_tree, variants_list)
-
-	all_staffGrp = MEI_tree.getDescendantsByName('staffGrp')
-	for i in alternates_list:
-		pass
+	local_alternatives(MEI_tree, alternates_list, EMENDATION)
 
