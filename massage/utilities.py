f0d9166868aaa93dee2d6f1f4598764d1fb020e9
Zoltan Komives <zolaemil@gmail.com>
2013-12-10 12:31:34 +0100

    moved chain_elems to utilities

--- massage/utilities.py
+++ massage/utilities.py
@@ -1,4 +1,6 @@
 
+from pymei import MeiElement
+
 def has_C_clef(staffGrp):
 	for staffDef in staffGrp.getChildren():
 		if staffDef.getAttribute('clef.shape').getValue() == 'C':
@@ -7,3 +9,16 @@ def has_C_clef(staffGrp):
 
 def get_all_staves(MEI_tree):
 	return MEI_tree.getDescendantsByName('staff')
+
+def chain_elems(start_elem, elems):
+	def getOrAddChild(mei_elem, child_name):
+		children = mei_elem.getChildrenByName(child_name)
+		if len(children) > 0:
+			return children
+		mei_elem.addChild(MeiElement(child_name))
+		return mei_elem.getChildrenByName(child_name)
+
+	if elems == []:
+		return start_elem
+	children = getOrAddChild(start_elem, elems[0])
+	return chain_elems(children[0], elems[1:])
