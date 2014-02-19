6e0650a65fdf0625b1aebeec260f9e0f72187a08
Zoltan Komives <zolaemil@gmail.com>
2014-02-19 19:12:49 +0000

    replaced massage.py with massage_mei.py to avoid name conflict with package
    Added proper parameter parsing to massage_mei.py

---
+++ massage/massage_mei.py
@@ -0,0 +1,34 @@
+import sys
+import argparse
+from analyze.analyze import analyze as make_analysis
+from transform.transform import TransformData
+from transform.transform import transform as transform_mei
+from pymei import XmlImport, XmlExport
+
+
+def massage_mei(in_file, out_file): 
+    try:
+        analysis = make_analysis(in_file)
+        MEI_instructions = TransformData(
+            arranger_to_editor=True,
+            obliterate_incipit=analysis.first_measure_empty,
+            replace_longa=True,
+            editorial_resp=analysis.has_arranger_element,
+            alternates_list=analysis.alternates_list)    
+        old_MEI_doc = XmlImport.documentFromFile(in_file)
+        new_MEI_doc = transform_mei(old_MEI_doc, MEI_instructions)
+        XmlExport.meiDocumentToFile(new_MEI_doc, out_file)
+    except Exception as ex:
+        print ex
+        print "Error during massaging " + in_file
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='MEI-Massage a single file.')
+    parser.add_argument('in_file')
+    parser.add_argument('--out', dest='out_file')
+    args = parser.parse_args()
+    if not args.out_file:
+        args.out_file = args.in_file + '_msg.mei'
+    print "Massage file: " + args.in_file
+    massage_mei(args.in_file, args.out_file)
+    print "DONE."
