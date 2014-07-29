4d62a2e0f6d568f19610ce7e8cd9fcc5e5055903
Micah Walter <micahjwalter@gmail.com>
2014-07-29 17:21:33 -0400

    Added function to replace 2/2 with 4/2 (retaining cut time symbol)

--- massage/transform/longa.py
+++ massage/transform/longa.py
@@ -3,16 +3,16 @@ from constants import *
 # from pymei import MeiElement
 
 def longa(MEI_tree):
-	"""Changes duration of final note in each part to a longa."""
-	all_measures = MEI_tree.getDescendantsByName('measure')
-	last_measure = all_measures[-1]
-	last_staves = last_measure.getChildren()
-	for staff in last_staves:
-		all_last_notes = staff.getDescendantsByName('note')
-		try:
-			last_note_of_staff = all_last_notes[-1]
-			# The following line *replaces* the existing @dur.
-			last_note_of_staff.addAttribute('dur', LONGA)
-			last_note_of_staff.addAttribute('dur.ges', '4096p')
-		except IndexError:
-			pass
+    """Changes duration of final note in each part to a longa."""
+    all_measures = MEI_tree.getDescendantsByName('measure')
+    last_measure = all_measures[-1]
+    last_staves = last_measure.getChildren()
+    for staff in last_staves:
+        all_last_notes = staff.getDescendantsByName('note')
+        try:
+            last_note_of_staff = all_last_notes[-1]
+            # The following line *replaces* the existing @dur.
+            last_note_of_staff.addAttribute('dur', LONGA)
+            last_note_of_staff.addAttribute('dur.ges', '4096p')
+        except IndexError:
+            pass
