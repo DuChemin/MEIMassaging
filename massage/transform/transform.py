8f90a296fee63427ba88c9cd84624e4c91be223f
zolaemil <zoltan@MacZoltan-GU.local>
2014-08-04 16:50:54 +0200

    added missing import

--- massage/transform/transform.py
+++ massage/transform/transform.py
@@ -13,6 +13,7 @@ from ignored import ignored
 from cut_time import double_cut_time
 from beams import eliminate_bad_beams
 from syllables import remove_empty_syllables
+from annot import remove_annot_brackets
 
 from constants import *
 from utilities import source_name2NCName
