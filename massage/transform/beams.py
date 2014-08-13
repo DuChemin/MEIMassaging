5820e370a320b175680be9e863d9ad9293e6617d
Micah Walter <micahjwalter@gmail.com>
2014-08-13 15:46:06 -0400

    Revisions to singleton beam handling, possibly fixing segfault problem

--- massage/transform/beams.py
+++ massage/transform/beams.py
@@ -1,5 +1,8 @@
 def eliminate_bad_beams(MEI_tree):
-    """Removes all beams that contain a single note."""
+    """Removes all beams that contain a single note.
+    Warning: should not be used for staves containing
+    more than one layer.
+    """
 
     def is_singleton_beam(element):
         """Function returns True iff an MEI element is
@@ -13,30 +16,30 @@ def eliminate_bad_beams(MEI_tree):
             return False
 
     # Get all layers in the MEI file, and get its children as a
-    # list. We will go through this list one by one and construct
-    # a new list of children. After adding each element to the
-    # list, we remove it from its parent.
+    # list. We will go through this list one by one and add them
+    # to our new layer. Before adding each element to the new
+    # layer, we remove it from the old one.
     all_layers = MEI_tree.getDescendantsByName('layer')
     for layer in all_layers:
         old_layer_items = layer.getChildren()
-        new_layer_items = []
+        new_layer = createInstance('layer')
         for item in old_layer_items:
             # If the item in the list is a singleton beam, then
-            # we should not add the beam to the revised list of elements
-            # to add; instead, we should add the beam's children,
-            # of which there is probably only one.
+            # we should not add the beam to the new layer; instead,
+            # we should add the beam's children, of which there is
+            # probably only one.
             if is_singleton_beam(item):
                 beam_children = item.getChildren()
                 for child in beam_children:
-                    new_layer_items.append(child)
                     child.getParent().removeChild(child)
+                    new_layer.addChild(child)
             # If the item in the list is anything other than a
-            # singleton beam, we simply add it to our revised list.
+            # singleton beam, we simply add it to our new layer.
             else:
-                new_layer_items.append(item)
                 item.getParent().removeChild(item)
-        # Now that we have the complete revised list, we remove all
-        # children from the layer and add the children from the revised
-        # list one by one.
-        for item in new_layer_items:
-            layer.addChild(item)
+                new_layer.addChild(item)
+        # Now that we have the complete new layer, we remove the old
+        # one from its parent and add the new one.
+        staff = layer.getParent()
+        staff.deleteAllChildren(layer)
+        staff.addChild(new_layer)
