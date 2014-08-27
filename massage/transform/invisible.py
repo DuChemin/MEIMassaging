b773bf5b1ca9b7cbf42f3e63237d1f60a352c4b7
Micah Walter <micahjwalter@gmail.com>
2014-08-27 18:26:11 -0400

    Can't mei element lists

--- massage/transform/invisible.py
+++ massage/transform/invisible.py
@@ -6,19 +6,21 @@ def make_invisible_space(MEI_tree):
     <space> elements.
     """
 
-    all_items = (MEI_tree.getDescendantsByName('note') +
-                 MEI_tree.getDescendantsByName('rest') +
-                 MEI_tree.getDescendantsByName('mRest')
-                 )
-    for item in all_items:
-        try:
-            if item.getAttribute('visible').getValue() == 'false':
-                space = MeiElement('space')
-                attributes = item.getAttributes()
-                for attr in attributes:
-                    space.addAttribute(attr)
-                parent = item.getParent()
-                parent.removeChild(item)
-                parent.addChild(space)
-        except:
-            pass
+    notes = MEI_tree.getDescendantsByName('note')
+    rests = MEI_tree.getDescendantsByName('rest')
+    mRests = MEI_tree.getDescendantsByName('mRest')
+
+    for list_of_elements in [notes, rests, mRests]:
+        for item in list_of_elements:
+            try:
+                if item.getAttribute('visible').getValue() == 'false':
+                    space = MeiElement('space')
+                    attributes = item.getAttributes()
+                    for attr in attributes:
+                        space.addAttribute(attr)
+                    # If mRest, need to calculate duration here...
+                    parent = item.getParent()
+                    parent.removeChild(item)
+                    parent.addChild(space)
+            except:
+                pass
