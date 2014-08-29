939f1f9017ea88e3a1639740c791755634c24a70
Micah Walter <micahjwalter@gmail.com>
2014-08-29 14:32:14 -0400

    Reorganized; also, fixed potential issue of space not being added at correct location.

--- massage/transform/invisible.py
+++ massage/transform/invisible.py
@@ -6,29 +6,27 @@ def make_invisible_space(MEI_tree):
     <space> elements.
     """
 
-    notes = MEI_tree.getDescendantsByName('note')
-    rests = MEI_tree.getDescendantsByName('rest')
-    mRests = MEI_tree.getDescendantsByName('mRest')
+    all_note_rest = MEI_tree.getDescendantsByName('note rest')
+    all_mRest = MEI_tree.getDescendantsByName('mRest')
 
     # Replace notes and rests with spaces
-    for list_of_elements in [notes, rests]:
-        for item in list_of_elements:
-            try:
-                if item.getAttribute('visible').getValue() == 'false':
-                    space = MeiElement('space')
-                    attributes = item.getAttributes()
-                    for attr in attributes:
-                        # Don't add octave or pitch attributes to space
-                        if attr.getName() not in ['oct', 'pname']:
-                            space.addAttribute(attr)
-                    # If mRest, need to calculate duration here...
-                    parent = item.getParent()
-                    parent.removeChild(item)
-                    parent.addChild(space)
-            except:  # doesn't have attribute `visible`
-                pass
+    for item in all_note_rest:
+        try:
+            if item.getAttribute('visible').getValue() == 'false':
+                space = MeiElement('space')
+                attributes = item.getAttributes()
+                for attr in attributes:
+                    # Don't add octave or pitch attributes to space
+                    if attr.getName() not in ['oct', 'pname']:
+                        space.addAttribute(attr)
+                # If mRest, need to calculate duration here...
+                parent = item.getParent()
+                parent.addChildBefore(item, space)
+                parent.removeChild(item)
+        except:  # doesn't have attribute `visible`
+            pass
     # Replace mRests with nothing -- just remove them
-    for item in mRests:
+    for item in all_mRest:
         try:
             if item.getAttribute('visible').getValue() == 'false':
                 item.getParent().removeChild(item)
